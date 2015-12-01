9e004a6a6463fb854eb99079c05cede5ea29e955
Jeremy Gresham <greshjs@gmail.com>
2015-11-30 20:38:12 -0500

    trying something new...

---
+++ new.php
@@ -0,0 +1,12 @@
+<?php
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+$file = 'last.txt';
+
+file_put_contents($file, $ip);
+?>
